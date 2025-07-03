e1a483ae74bbc2e0d3e7e57ba158b539d2d0056d
sameer <sameerkhan9991303458@gmail.com>
2025-07-03 23:30:17 +0530

    abc

--- src/pages/admin-login.js
+++ src/pages/admin-login.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Button, ButtonBase, Grid, Paper, TextField } from '@mui/material';
 import '../App.css';
-
+import { Link } from 'react-router-dom';
 export default function adminlogin() {
   document.title = "Admin Login";
   return (
-    <Box className="adminLoginPage">
-      
+    <Box className="adminLogin">
+      <Box className="formcontainer">
+        <p className="title">Admin Login</p>
+        <form className="form">
+          <Box className="inputgroup">
+            <label for="email">Email Address / UserId</label>
+            <input type="email" name="email" id="email" required placeholder="" />
+          </Box>
+          <Box className="inputgroup">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" required placeholder="" />
+          </Box>
+          <button className="sign"><Link to="#">Login </Link></button>
+        </form>
+      </Box>
     </Box>
   )
 }
